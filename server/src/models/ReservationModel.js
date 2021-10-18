23021a5706ae740b57c1361a07147bc668f19af5
Kylewuzhere1000 <kylevasquez@developersinstitute.co.nz>
2021-10-18 16:07:47 +1300

    transformed _id prop to id in restaurant and reservation models

--- server/src/models/ReservationModel.js
+++ server/src/models/ReservationModel.js
@@ -1,11 +1,22 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const reservationSchema = new Schema({
-  partySize: { type: Number, required: true },
-  date: { type: Date, required: true },
-  userId: { type: String, required: true },
-  restaurantName: { type: String, required: true },
-});
+const reservationSchema = new Schema(
+  {
+    partySize: { type: Number, required: true },
+    date: { type: Date, required: true },
+    userId: { type: String, required: true },
+    restaurantName: { type: String, required: true },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 module.exports = mongoose.model("Reservation", reservationSchema);
--- server/src/models/RestaurantModel.js
+++ server/src/models/RestaurantModel.js
@@ -1,10 +1,21 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const restaurantSchema = new Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  image: { type: String, required: true },
-});
+const restaurantSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 module.exports = mongoose.model("Restaurant", restaurantSchema);
