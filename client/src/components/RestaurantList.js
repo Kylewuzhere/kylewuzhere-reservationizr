ff84b0edbf1a1bdecaeda2c2dd98473f6632abd1
Kylewuzhere1000 <kylevasquez@developersinstitute.co.nz>
2021-10-18 17:41:05 +1300

    imported React, useState and useEffect hooks to fetch restaurants data and map data into app

--- client/src/components/RestaurantList.js
+++ client/src/components/RestaurantList.js
@@ -1,9 +1,32 @@
 import "./RestaurantList.css";
+import React, { useState, useEffect } from "react";
 
 const RestaurantList = () => {
+  const [restaurantList, setRestaurantList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(`http://localhost:5000/restaurants`);
+
+      const data = await res.json();
+      setRestaurantList(data);
+    };
+    fetchData();
+  }, []);
   return (
     <>
       <h1>Restaurants</h1>
+      <div className="restaurants-container">
+        {restaurantList.map((res) => {
+          return (
+            <li key={res.id}>
+              <img src={res.image} alt={res.name} />
+              <h2 className="restaurant-name">{res.name}</h2>
+              <p className="restaurant-description">{res.description}</p>
+            </li>
+          );
+        })}
+      </div>
     </>
   );
 };
