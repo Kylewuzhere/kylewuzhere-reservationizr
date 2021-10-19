c760d54c28cb8806258a92a227bc1fbb033b5dcd
Kylewuzhere1000 <kylevasquez@developersinstitute.co.nz>
2021-10-19 15:36:59 +1300

    imported RestaurantModel to use query restaurant data; created a GET /restaurants endpoint to retrieve restaurant data

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const RestaurantModel = require("./models/RestaurantModel");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/restaurants", async (req, res) => {
+  const restaurants = await RestaurantModel.find({});
+  return res.status(200).send(restaurants);
+});
 
 module.exports = app;
