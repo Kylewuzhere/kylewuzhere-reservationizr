a5c5d7b3fa6e878f081cc02e9b15457a8a8196e4
Kylewuzhere1000 <kylevasquez@developersinstitute.co.nz>
2021-10-20 14:44:22 +1300

    added 400 and 404 error response with id validation

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const RestaurantModel = require("./models/RestaurantModel");
+const validId = require("./utils/validId");
 const app = express();
 
 app.use(cors());
@@ -13,8 +14,19 @@ app.get("/restaurants", async (req, res) => {
 
 app.get("/restaurants/:id", async (req, res) => {
   const { id } = req.params;
-  const restaurant = await RestaurantModel.findById(id);
-  return res.status(200).send(restaurant);
+
+  if (validId(id)) {
+    const restaurant = await RestaurantModel.findById(id);
+    if (restaurant === null) {
+      return res
+        .status(404)
+        .send("The restaurant trying to be retrieved does not exist");
+    } else {
+      res.status(200).send(restaurant);
+    }
+  } else {
+    res.status(400).send("Invalid ID is provided");
+  }
 });
 
 module.exports = app;
