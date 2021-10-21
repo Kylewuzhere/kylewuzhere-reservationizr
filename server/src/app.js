7dae8a3efd45c392ed713681310845ece885e97c
Kylewuzhere1000 <kylevasquez@developersinstitute.co.nz>
2021-10-21 22:40:59 +1300

    imported celebrate middleware to validate POST requests with joi schema

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const { celebrate, errors, Segments } = require("celebrate");
 const RestaurantModel = require("./models/RestaurantModel");
 const ReservationModel = require("./models/ReservationModel");
+const reserveValidSchema = require("./models/reserveValidSchema");
 const validId = require("./utils/validId");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/reservations", async (req, res) => {
-  const { body } = req;
-  const reservation = new ReservationModel(body);
-  await reservation.save();
-  res.status(201).send(reservation);
-});
+app.post(
+  "/reservations",
+  celebrate({ [Segments.BODY]: reserveValidSchema }),
+  async (req, res, next) => {
+    try {
+      const { body } = req;
+      const reservation = new ReservationModel(body);
+      await reservation.save();
+      res.status(201).send(reservation);
+    } catch (err) {
+      err.status = 400;
+      next(err);
+    }
+  }
+);
 
 app.get("/restaurants", async (req, res) => {
   const restaurants = await RestaurantModel.find({});
@@ -36,5 +47,7 @@ app.get("/restaurants/:id", async (req, res) => {
     res.status(400).send("Invalid ID is provided");
   }
 });
+
+app.use(errors());
 
 module.exports = app;
