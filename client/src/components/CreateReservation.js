480fbe73fe5e727563d7e4001c70982c9a7880b9
Kylewuzhere1000 <kylevasquez@developersinstitute.co.nz>
2021-10-22 21:19:49 +1300

    Fetched data from restaurant list by id; basic JSX added to create form page with partySize and date inputs; Reservation submission handle by making a POST request to /reservations; added error state to handle error responses; imported useAuth0 to request for access token to submit reservations from user accounts

--- client/src/components/CreateReservation.js
+++ client/src/components/CreateReservation.js
@@ -1,14 +1,102 @@
 import DatePicker from "react-datepicker";
 import { useParams, useHistory } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import "react-datepicker/dist/react-datepicker.css";
 import "./CreateReservation.css";
 
 const CreateReservation = () => {
   const { restaurantId } = useParams();
+  const [restaurant, setRestaurant] = useState({});
+  const [partySize, setPartySize] = useState("");
+  const [date, setDate] = useState(new Date());
+  const [isError, setIsError] = useState(false);
+  const [errorStatus, setErrorStatus] = useState(false);
+  const history = useHistory();
+  const { getAccessTokenSilently } = useAuth0();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        `http://localhost:5000/restaurants/${restaurantId}`
+      );
+
+      const data = await res.json();
+      setRestaurant(data);
+    };
+    fetchData();
+  }, [restaurantId]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const body = {
+      partySize: partySize,
+      date: date,
+      restaurantName: restaurant.name,
+    };
+
+    const accessToken = await getAccessTokenSilently();
+    const response = await fetch("http://localhost:5000/reservations", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!response.ok) {
+      setIsError(true);
+      console.log(response);
+      setErrorStatus(response.status);
+    } else {
+      history.push("/");
+    }
+  };
+
+  if (isError) {
+    return (
+      <>
+        <p className="no-reserve">
+          {errorStatus} Error creating a reservation.
+        </p>
+        <p>Party size must be greater than or equal to one.</p>
+        <p>Date must be greater than or equal to the current date.</p>
+      </>
+    );
+  }
 
   return (
     <>
-      <h1>Reserve Restaurant</h1>
+      <div>
+        <h1>Reserve {restaurant.name}</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-field">
+            <label htmlFor="partySize">Number of guests</label>
+            <input
+              type="number"
+              id="partySize"
+              min="0"
+              value={partySize}
+              onChange={(e) => setPartySize(e.target.value)}
+              required
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="date">Date</label>
+            <DatePicker
+              id="date"
+              selected={date}
+              onChange={(e) => setDate(e)}
+              showTimeSelect
+              dateFormat="MM/dd/yyyy, h:mm aa"
+              required
+            />
+          </div>
+
+          <button className="btn-general">Submit</button>
+        </form>
+      </div>
     </>
   );
 };
