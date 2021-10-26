d39fe45810e77af181ee9144ed29b2242b297193
Kylewuzhere1000 <kylevasquez@developersinstitute.co.nz>
2021-10-26 22:20:31 +1300

    fetch reservation data in ReservtionList.js to display reservations in web app

--- client/src/components/ReservationList.js
+++ client/src/components/ReservationList.js
@@ -1,10 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 import "./ReservationList.css";
 import { formatDate } from "../utils/formatDate";
 
 const ReservationList = () => {
+  const [reservationList, setReservationList] = useState([]);
+  const { getAccessTokenSilently } = useAuth0();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const accessToken = await getAccessTokenSilently();
+      const response = await fetch(`http://localhost:5000/reservations`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      const data = await response.json();
+      setReservationList(data);
+    };
+    fetchData();
+  }, [getAccessTokenSilently]);
+
   return (
     <>
       <h1>Upcoming reservations</h1>
+      <div className="reserve-cont">
+        {reservationList.map(({ id, restaurantName, date }) => {
+          return (
+            <li className="reservation" key={id}>
+              <strong>{restaurantName}</strong>
+              <p className="reserve-date">{formatDate(date)}</p>
+              <Link>View Details &rarr;</Link>
+            </li>
+          );
+        })}
+      </div>
     </>
   );
 };
